b91e5ac1a7d0d870511a23048275e1015c9dd268
Ayush P Vachhani <ayupatel107@gmail.com>
2024-02-14 10:14:09 +0530

    Basic svg shapes updated.

---
+++ NeuroFlow_UI/src/lib/SVG/Shapes/Circle.svelte
@@ -0,0 +1,4 @@
+<script>
+    export let cx, cy, r, stroke="black", fill="none", strokeWidth;
+</script>
+<circle cx={cx} cy={cy} r={r} stroke={stroke} fill={fill} stroke-width={strokeWidth}/>
---
+++ NeuroFlow_UI/src/lib/SVG/Shapes/Rectangle.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Circle from "$lib/SVG/Shapes/Circle.svelte";
+    //Child circle components
+    export let No_of_Circles = 5;
+    export let width = 120;
+    let rectangle_height = 50 * No_of_Circles;
+    //Reactive statements
+    $:No_of_Circles;
+    $:rectangle_height = 50 * No_of_Circles;
+
+</script>
+
+<svg width={width} height={rectangle_height + 20}>
+    <rect x="10" y="15" width="100" height={rectangle_height} stroke="black" fill="none" stroke-width="1"/>
+    {#each Array.from({ length: No_of_Circles }) as _, i}
+        <Circle cx={60} cy={40 + 50*i } r="20" />
+    {/each}
+</svg>
