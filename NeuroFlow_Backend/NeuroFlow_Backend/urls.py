fe807d4fbc8bca24dc61678204664bbbfdbcfecb
Ayush-vachhani <ayupatel107@gmail.com>
2024-03-29 18:15:41 +0530

    Updated urls.

--- NeuroFlow_Backend/NeuroFlow_Backend/urls.py
+++ NeuroFlow_Backend/NeuroFlow_Backend/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views.fetch_files import FileListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/files/', FileListView.as_view()),
 ]
