a1d2e34fb82e52b8de4609e38598ffba8edfafad
Ayush-vachhani <ayupatel107@gmail.com>
2024-03-29 18:26:12 +0530

    Updated run files.

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 # Starting Backend
 echo "Starting Backend..."
-gnome-terminal -- bash -c "cd NeuroFlow_Backend; source .venv/bin/activate; python manage.py runserver; exec bash"
+gnome-terminal -- bash -c "cd NeuroFlow_Backend; source venv/bin/activate; python manage.py runserver; exec bash"
 
 # Starting UI
 echo "Starting UI..."
-gnome-terminal -- bash -c "cd UI; pnpm run dev; exec bash"
+gnome-terminal -- bash -c "cd UI; pnpm dev; exec bash"
 
 echo "Servers are starting..."
